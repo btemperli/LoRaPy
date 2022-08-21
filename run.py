5a729831435b6e6a8fb4030089db4f24cc79c6e8
Devin Gaffney <itsme@devingaffney.com>
2022-08-21 12:37:49 -0700

    updates

--- run.py
+++ run.py
@@ -33,29 +33,29 @@ width = display.width
 height = display.height
 lora = HeliumLoRa.init(True)
 lora.otaa()
-def run():
-    lora = HeliumLoRa.init()
-    if not keys.nwskey:
-        devaddr, nwskey, appskey = lora.otaa()
-    while True:
-        time.sleep(.1)
-        display.fill(0)
-        display.text("Test is "+str(lora.test_status["running_ping"]), 0, 0, 1)
-        display.text('Time: '+str(lora.test_status["last_message"]), 0, 10, 1)
-        display.text('Total Pings: '+str(lora.test_status["ping_count"]), 0, 20, 1)
-        display.show()
-        if lora.test_status["running_ping"] and (datetime.datetime.now() - lora.last_test).seconds > 5:
-            lora.transact("Test")
-        if not btnA.value:
-            lora.test_status["running_ping"] = True
-        if not btnB.value:
-            lora.test_status["running_ping"] = False
-        if not btnC.value:
-            display.fill(0)
-            display.text("Test is shut down!", 0, 0, 1)
-            display.text('Must restart PI to', 0, 10, 1)
-            display.text('restart test.', 0, 20, 1)
-            display.show()
-            raise KeyboardInterrupt
-
-run()
+# def run():
+#     lora = HeliumLoRa.init()
+#     if not keys.nwskey:
+#         devaddr, nwskey, appskey = lora.otaa()
+#     while True:
+#         time.sleep(.1)
+#         display.fill(0)
+#         display.text("Test is "+str(lora.test_status["running_ping"]), 0, 0, 1)
+#         display.text('Time: '+str(lora.test_status["last_message"]), 0, 10, 1)
+#         display.text('Total Pings: '+str(lora.test_status["ping_count"]), 0, 20, 1)
+#         display.show()
+#         if lora.test_status["running_ping"] and (datetime.datetime.now() - lora.last_test).seconds > 5:
+#             lora.transact("Test")
+#         if not btnA.value:
+#             lora.test_status["running_ping"] = True
+#         if not btnB.value:
+#             lora.test_status["running_ping"] = False
+#         if not btnC.value:
+#             display.fill(0)
+#             display.text("Test is shut down!", 0, 0, 1)
+#             display.text('Must restart PI to', 0, 10, 1)
+#             display.text('restart test.', 0, 20, 1)
+#             display.show()
+#             raise KeyboardInterrupt
+#
+# run()
