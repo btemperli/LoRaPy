fbcdce420cbf57b35ee5ca9270cf09e005a9d7b5
Devin Gaffney <itsme@devingaffney.com>
2022-08-21 12:39:53 -0700

    updates

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
 # Clear the display.
 width = display.width
 height = display.height
-lora = HeliumLoRa.init(True)
+lora = HeliumLoRa.init(True, True, True)
 lora.otaa()
 # def run():
 #     lora = HeliumLoRa.init()
