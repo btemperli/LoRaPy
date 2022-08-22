1d8ae3dee8dc343ef6e23b8fda9e331022bec297
Devin Gaffney <itsme@devingaffney.com>
2022-08-21 18:04:19 -0700

    updates

--- run.py
+++ run.py
@@ -3,10 +3,13 @@ import json
 from helium import Helium
 from gps import get_gps_data
 helium = Helium()
-helium.transact({"balh": 1})
-while True:
+def fire_ping():
     gps_data = get_gps_data()
     response = {}
     if gps_data:
-        response = helium.transact(json.dumps(gps_data))
+        response = helium.transact(json.dumps(gps_data)) or {}
+    return response
+
+while True:
+    response = fire_ping()
     time.sleep(response.get("next_ping_at", 10))
