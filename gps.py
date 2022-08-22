83f762530eb5c06397c77b95615b4b36f3ef9cb8
Devin Gaffney <itsme@devingaffney.com>
2022-08-21 17:25:28 -0700

    updates

---
+++ gps.py
@@ -0,0 +1,12 @@
+import os
+import serial
+import adafruit_gps
+uart = serial.Serial(os.popen("ls /dev/ttyUSB*").read().strip(), baudrate=9600, timeout=10)
+gps = adafruit_gps.GPS(uart, debug=False)
+gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
+gps.send_command(b'PMTK220,2000')
+last_print = time.monotonic()
+def get_gps_data():
+    gps.update()
+    if gps.has_fix:
+        return {"lat": gps.latitude, "lon": gps.longitude, "speed": gps.speed_knots, "alt": gps.altitude_m}
