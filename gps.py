92163330fc31f8afc3708e31a8d64f084258cf1e
Devin Gaffney <itsme@devingaffney.com>
2022-08-23 13:25:40 -0700

    updates

--- gps.py
+++ gps.py
@@ -1,12 +1,7 @@
-import os
-import serial
-import adafruit_gps
-import time
-uart = serial.Serial(os.popen("ls /dev/ttyUSB*").read().strip(), baudrate=9600, timeout=10)
-gps = adafruit_gps.GPS(uart, debug=False)
-gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
-gps.send_command(b"PMTK220,1000")
+from gpsdclient import GPSDClient
+
+client = GPSDClient(host="127.0.0.1")
 def get_gps_data():
-    gps.update()
-    if gps.has_fix:
-        return {"lat": gps.latitude, "lon": gps.longitude, "speed": gps.speed_knots, "alt": gps.altitude_m}
+    for result in client.dict_stream(convert_datetime=True):
+        if result["class"] == "TPV":
+            return {"lat": result.get("lat"), "lon": result.get("lon"), "speed": result.get("speed"), "alt": result.get("alt")}
