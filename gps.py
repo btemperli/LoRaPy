3267728721acd4474e079a3373e9b60125773ebd
Devin Gaffney <itsme@devingaffney.com>
2022-08-23 13:27:16 -0700

    updates

--- gps.py
+++ gps.py
@@ -1,7 +1,10 @@
+import datetime
 from gpsdclient import GPSDClient
 
 client = GPSDClient(host="127.0.0.1")
-def get_gps_data():
+def get_gps_data(transact_timeout=10):
+    start = datetime.datetime.now()
     for result in client.dict_stream(convert_datetime=True):
+        (datetime.datetime.now() - start).seconds < transact_timeout
         if result["class"] == "TPV":
             return {"lat": result.get("lat"), "lon": result.get("lon"), "speed": result.get("speed"), "alt": result.get("alt")}
