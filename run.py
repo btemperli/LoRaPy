d4c7d543ce4c4059251ec05deeda3839bc36236c
Devin Gaffney <itsme@devingaffney.com>
2022-08-23 12:33:11 -0700

    more safety checks

--- run.py
+++ run.py
@@ -2,14 +2,23 @@ import time
 import json
 from helium import Helium
 from gps import get_gps_data
-helium = Helium()
 def fire_ping():
     gps_data = get_gps_data()
-    response = {}
-    if gps_data:
-        response = helium.transact(json.dumps(gps_data)) or {}
-    return response
+    return helium.transact(json.dumps(gps_data or {})) or {}
+
+helium = None
+while not helium:
+    try:
+        helium = Helium()
+    except:
+        helium = None
+        time.sleep(5)
 
 while True:
-    response = fire_ping()
-    time.sleep(response.get("next_ping_at", 10))
+    try:
+        response = fire_ping()
+        print(response)
+        time.sleep(response.get("next_ping_at", 10))
+    except:
+        print("oops")
+        time.sleep(10)
