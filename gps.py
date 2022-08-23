b1d75e58d1ae4e2af861aa853df192767830e013
Devin Gaffney <itsme@devingaffney.com>
2022-08-23 12:48:51 -0700

    back to defaults

--- gps.py
+++ gps.py
@@ -4,9 +4,8 @@ import adafruit_gps
 import time
 uart = serial.Serial(os.popen("ls /dev/ttyUSB*").read().strip(), baudrate=9600, timeout=10)
 gps = adafruit_gps.GPS(uart, debug=False)
-gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
-gps.send_command(b'PMTK220,2000')
-last_print = time.monotonic()
+gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+gps.send_command(b"PMTK220,1000")
 def get_gps_data():
     gps.update()
     if gps.has_fix:
